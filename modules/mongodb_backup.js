ef2c864a7a07ec918853fc3f6f554305ed7f67b0
mckk84 <mckk84@users.noreply.github.com>
2023-01-08 16:08:35 +0530

    Removed unwaned

--- modules/mongodb_backup.js
+++
@@ -1,5 +0,0 @@
-const { MongoClient } = require('mongodb');
-const URI = "mongodb+srv://charan:MNIAtrQJAe8W5eIQ@cluster0.07bxow4.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(URI);
-
-module.exports = { client }; 
